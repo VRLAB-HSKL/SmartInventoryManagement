d86e50ad8572a34ba243a0a8d3cbed4ad7c78126
Lukas Felzmann <lukas.felzmann@gmx.de>
2022-06-07 14:14:26 +0200

    GUI für Sign up implementiert (ohne Request-Logik)

--- frontend/webapp/src/app/authentication/sign-up-form/sign-up-form.component.ts
+++ frontend/webapp/src/app/authentication/sign-up-form/sign-up-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up-form.component.css']
 })
 export class SignUpFormComponent implements OnInit {
+  @ViewChild("form") form!: NgForm;
+  @ViewChild("password") password!: FormControl;
+  @ViewChild("confirmPassword") confirmPassword!: FormControl;
+  passwordFieldType: string = "password";
+  error: string | null = "";
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changePasswordVisibility(){
+    if(this.passwordFieldType === "password"){
+      this.passwordFieldType = "text";
+    } else if(this.passwordFieldType === "text"){
+      this.passwordFieldType = "password";
+    } else{
+      throw Error(`passwordFieldType has entered an invalid state. passwordFieldType=${this.passwordFieldType}`);
+    }
+  }
+
+  onSubmit(form: NgForm){
+    let nickname: string | null = form.value.nickname;
+    let email = form.value.email;
+    let password = form.value.password;
+    
+    console.log(`nickname=${nickname}, email=${email}, password=${password}`); // Todo: Den Log raus und dafür die Request-Logik rein.
+
+    form.reset();
+  }
+
+  checkIfPasswordsMatch(){
+    const passwordsMatch: boolean = this.password.value === this.confirmPassword.value;
+    if(passwordsMatch){
+      this.form.controls['confirmPassword'].setErrors(null);
+    } else{
+      this.form.controls['confirmPassword'].setErrors({'incorrect': true});
+    }
+  }
+
 }
+
