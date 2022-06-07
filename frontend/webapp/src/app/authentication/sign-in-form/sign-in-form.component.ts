255cbe402789fcf721f302e5a632b0ad284548d8
Lukas Felzmann <lukas.felzmann@gmx.de>
2022-06-07 16:44:10 +0200

    GUI für Sign in (ohne Request-Logik) implementiert

--- frontend/webapp/src/app/authentication/sign-in-form/sign-in-form.component.ts
+++ frontend/webapp/src/app/authentication/sign-in-form/sign-in-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormControl, NgForm, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in-form',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in-form.component.css']
 })
 export class SignInFormComponent implements OnInit {
+  @ViewChild("form") form!: NgForm;
+  passwordFieldType: string = "password";
+  error: string | null = "";
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changePasswordVisibility(){
+    if(this.passwordFieldType === "password"){
+      this.passwordFieldType = "text";
+    } else if(this.passwordFieldType === "text"){
+      this.passwordFieldType = "password";
+    } else{
+      throw Error(`passwordFieldType has entered an invalid state. passwordFieldType=${this.passwordFieldType}`);
+    }
+  }
+
+  onSubmit(form: NgForm){
+    let email = form.value.email;
+    let password = form.value.password;
+
+    console.log(`email=${email}, password=${password}`); // Todo: Den Log raus und dafür die Request-Logik rein.
+
+    form.reset();
   }
 
 }
