f058f5b1736d0a9ebc959c1c497c15878c11b4e1
Lukas Felzmann <lukas.felzmann@gmx.de>
2022-06-07 16:37:03 +0200

    Component für Sign-in-Form generiert

--- frontend/webapp/src/app/app.module.ts
+++ frontend/webapp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SignUpFormComponent } from './authentication/sign-up-form/sign-up-form.component';
+import { SignInFormComponent } from './authentication/sign-in-form/sign-in-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpFormComponent
+    SignUpFormComponent,
+    SignInFormComponent
   ],
   imports: [
     BrowserModule,
